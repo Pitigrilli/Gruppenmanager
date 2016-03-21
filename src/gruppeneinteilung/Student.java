27e516d83f4755dff96ff6d96e4448aa8c2e39b5
Familie <Familie@192.168.2.107>
2016-03-21 10:30:49 +0100

    Attribut Jahrgang eingefügt, Konstruktor und bisherigen Methoden angepasst, zu allen Attributen get-Methoden geschrieben, zu Klasse,Jahrgang,Religion set-Methoden geschrieben,

--- src/gruppeneinteilung/Student.java
+++ src/gruppeneinteilung/Student.java
@@ -10,18 +10,20 @@ package gruppeneinteilung;
  * @author behl.claus
  */
 public class Student implements Comparable<Student>{
-    private final String klasse;
+    private String klasse;
+    private String jahrgang;
     private final String name;
     private final String geschlecht;
     private final String zweig;
     private final String fs2;
     private final String fs3;
     private final String fs4;
-    private final String religion;
+    private String religion;
     
    
-    public Student(String klasse, String name, String geschlecht, String zweig, String fs2, String fs3, String fs4, String religion) {
+    public Student(String klasse,String jahrgang, String name, String geschlecht, String zweig, String fs2, String fs3, String fs4, String religion) {
         this.klasse = klasse;
+        this.jahrgang = jahrgang;
         this.name = name;
         this.geschlecht = geschlecht;
         this.zweig = zweig;
@@ -42,9 +44,49 @@ public class Student implements Comparable<Student>{
     }
     
     public String toString(){
-        return klasse+" "+name+" "+geschlecht+" "+zweig+" "+fs2+" "+fs3+" "+fs4+" "+religion;
+        return klasse+" "+jahrgang+" "+name+" "+geschlecht+" "+zweig+" "+fs2+" "+fs3+" "+fs4+" "+religion;
+    }
+    public String getKlasse(){
+         return klasse;
+     }
+    public String getJahrgang(){
+        return jahrgang;
+    }
+    public String getGeschlecht(){
+        return geschlecht;
     }
     
+    public String getZweig(){
+        return zweig;
+    }
+    
+    public String getFs2(){
+        return fs2;
+    }
+    
+    public String getFs3(){
+        return fs3;
+    }
+    
+    public String getFs4(){
+        return fs4;
+    }
+    
+    public String getReligion(){
+        return religion;
+    }
+    
+    public void setKlasse(String klasse){
+        this.klasse = klasse;
+    }
+    
+    public void setReligion(String religion){
+       this.religion = religion;
+    }
+    
+    public void setJahrgang(String jahrgang){
+        this.jahrgang = jahrgang;
+    }
  }
     
 
