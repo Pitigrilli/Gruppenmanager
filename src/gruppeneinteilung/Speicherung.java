9fb5cb42b654bbcc3f59fe6d696b1aa961151ae5
behl.claus <behl.claus@jack-steinberger-gymnasium.de>
2016-04-25 16:42:13 +0200

    Datei Auswahldialog hinzugefügt.

--- src/gruppeneinteilung/Speicherung.java
+++ src/gruppeneinteilung/Speicherung.java
@@ -7,80 +7,114 @@ package gruppeneinteilung;
 
 import java.io.*;
 import java.util.ArrayList;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  *
  * @author mueller.fabian
  */
 public class Speicherung {
-    
-    public static void main(String[] args){
-        Speicherung sp = new Speicherung(new Gruppeneinteilung("ASV.csv"));
+
+    public static void main(String[] args) {
+        //Speicherung sp = new Speicherung(new Gruppeneinteilung("ASV.csv"));
         //sp.serialisieren();
-        System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-        sp.serialisierungLaden();
-        sp.ge.testeEinteilung();
+        Speicherung sp = new Speicherung();
+        sp.testeLaden();
     }
     Gruppeneinteilung ge;
+    File file;
     
-    public Speicherung(Gruppeneinteilung ge){
-        this.ge=ge;
+
+    public Speicherung(Gruppeneinteilung ge) {
+        this.ge = ge;
+        waehleFile();
+    }
+
+    public Speicherung(String filename) {
+        ge = new Gruppeneinteilung(0);
     }
     
-    public  void serialisieren(){
-        
-        try{
-            FileOutputStream fileStream = new FileOutputStream("serialisierung.ser");
+    public Speicherung(){
+        waehleFile();
+    }
+
+    public void serialisieren() {
+
+        try {
+            FileOutputStream fileStream = new FileOutputStream(file);
             ObjectOutputStream objectStream = new ObjectOutputStream(fileStream);
             objectStream.writeObject(ge.students);
             objectStream.writeObject(ge.jahrgaenge);
-        } catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
-    
-    public  void serialisierungLaden(){
-        try{
-            FileInputStream fileStream = new FileInputStream("serialisierung.ser");
+
+    public Gruppeneinteilung serialisierungLaden() {
+        
+        try {
+            FileInputStream fileStream = new FileInputStream(file);
             ObjectInputStream objectStream = new ObjectInputStream(fileStream);
             Object obj = objectStream.readObject();
             if (obj instanceof ArrayList<?>) {
-                 ArrayList<?> al = (ArrayList<?>) obj;
-                    if (al.size() > 0) {
-                     // Iterate.
-                         for (int i = 0; i < al.size(); i++) {
-                          // Still not enough for a type.
+                ArrayList<?> al = (ArrayList<?>) obj;
+                if (al.size() > 0) {
+                    // Iterate.
+                    for (int i = 0; i < al.size(); i++) {
+                        // Still not enough for a type.
                         Object o = al.get(i);
-                              if (o instanceof Student) {
-                                    Student v = (Student) o;
-                                    ge.students.add(v);
-                                }
+                        if (o instanceof Student) {
+                            Student v = (Student) o;
+                            ge.students.add(v);
                         }
                     }
+                }
             }
             obj = objectStream.readObject();
             if (obj instanceof ArrayList<?>) {
-                 ArrayList<?> al = (ArrayList<?>) obj;
-                    if (al.size() > 0) {
-                     // Iterate.
-                         for (int i = 0; i < al.size(); i++) {
-                          // Still not enough for a type.
+                ArrayList<?> al = (ArrayList<?>) obj;
+                if (al.size() > 0) {
+                    // Iterate.
+                    for (int i = 0; i < al.size(); i++) {
+                        // Still not enough for a type.
                         Object o = al.get(i);
-                              if (o instanceof Jahrgang) {
-                                    Jahrgang v = (Jahrgang) o;
-                                    ge.jahrgaenge.add(v);
-                                }
+                        if (o instanceof Jahrgang) {
+                            Jahrgang v = (Jahrgang) o;
+                            ge.jahrgaenge.add(v);
                         }
                     }
+                }
             }
                 //ge.students = (ArrayList<Student>) objectStream.readObject();
-                //ge.jahrgaenge = (ArrayList<Jahrgang>) objectStream.readObject();
-            
-            
-            
-        } catch(Exception ex){
+            //ge.jahrgaenge = (ArrayList<Jahrgang>) objectStream.readObject();
+
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
+        return ge;
     }
     
+    
+    public void waehleFile(){
+        JFileChooser chooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                "GED-Datei", "ged");
+        chooser.setFileFilter(filter);
+        int returnVal;
+        returnVal = chooser.showOpenDialog(null);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            //System.out.println("You chose to open this file: "
+            //        + chooser.getSelectedFile().getName());
+            file = chooser.getSelectedFile();
+        }
+    }
+
+    public void testeLaden() {
+        System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+        serialisierungLaden();
+        ge.testeEinteilung();
+
+    }
+
 }
